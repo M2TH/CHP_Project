6e8788a3985ec136f25ef550ed3ce3626d7c2a40
Tristan522 <80270075+Tristan522@users.noreply.github.com>
2023-04-11 16:37:14 +0200

    Add files via upload

---
+++ Projet_Tris_0404/Projet_Tris_1104/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+#include "fonction.h"
+ 
+
+
+int main()
+{
+    int Nx,Ny;
+    double alpha,beta,gamma,D,Lx,Ly,deltax,deltay,deltat;
+    double x,y,t;
+    double res;
+    
+    x=2; 
+    y=1;
+    t=1;
+    Lx=1;
+    Ly=1;
+    Nx=3;
+    Ny=4;
+    deltax=Lx/Nx;
+    deltay=Ly/Ny;
+    deltat=1;
+    alpha=1+(2/(deltax*deltax))*(2/(deltay*deltay));
+    beta=-1/(deltax*deltax);
+    gamma=-1/(deltay*deltay);
+
+    vector<double> X;
+    res=f_cas1(x,y,t,Lx,Ly);
+
+    cout<<res<<endl;
+
+
+
+
+
+
+    return 0;
+}
+
