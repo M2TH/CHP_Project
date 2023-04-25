6039d8931aa9825c50e3453541e15b7c93bc793f
M2TH <116036554+M2TH@users.noreply.github.com>
2023-04-25 14:25:51 +0200

    Delete Projet_2004 directory

--- Projet_2004/gradConj.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-using namespace std;
-typedef vector<double> Vector;
-double dot(const Vector& x, const Vector& y);
-double norm(const Vector& x) ;
-vector<double> gradientConj ( Vector& K, double tol, int maxiter);
-
-
